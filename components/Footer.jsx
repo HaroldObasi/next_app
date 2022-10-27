0a3a00e4cae82b75c27fbbbe6a9eeef9a462b1cf
HaroldObasi <haroldobasi2k16@gmail.com>
2022-10-27 11:15:39 +0100

    changed some responsive format

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,15 @@
 const Footer = () => {
     return (
-        <footer className="hidden lg:flex justify-between py-10 mt-[100px] mx-[113px] border-t-2">
-            <img src="/zuriLogo.svg" alt="" />
-            <p className="text-gray-500 font-inter">
+        <footer className="flex md:flex-row flex-col justify-between py-10 mt-[100px] lg:mx-[113px] mx-5 border-t-2">
+            <img
+                src="/zuriLogo.svg"
+                alt=""
+                className="md:w-[170px] w-[120px]"
+            />
+            <p className="text-gray-500 font-inter py-3 md">
                 HNG Internship 9 Frontend Task
             </p>
-            <img src="/I4G.png" alt="" />
+            <img src="/I4G.png" alt="" className="md:w-[170px] w-[120px]" />
         </footer>
     );
 };
